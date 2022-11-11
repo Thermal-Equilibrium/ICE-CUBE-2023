e04c0e8cd04dba93c259671192e58e45b1d06649
Ayush <61765149+citricacid2@users.noreply.github.com>
2022-11-11 02:11:07 -0500

    cool new teleop stuff (untested)

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/Commands/MiscCommands/MoveFieldCursor.java
@@ -0,0 +1,37 @@
+package org.firstinspires.ftc.teamcode.Robot.Commands.MiscCommands;
+
+import org.firstinspires.ftc.teamcode.CommandFramework.Command;
+import org.firstinspires.ftc.teamcode.Utils.VirtualField;
+
+public class MoveFieldCursor extends Command {
+    int x;
+    int y;
+    VirtualField virtualField;
+
+
+    public MoveFieldCursor(int x, int y, VirtualField virtualField) {
+        this.x = x;
+        this.y = y;
+        this.virtualField = virtualField;
+    }
+
+    @Override
+    public void init() {
+        virtualField.moveCursor(x, y);
+    }
+
+    @Override
+    public void periodic() {
+
+    }
+
+    @Override
+    public boolean completed() {
+        return true;
+    }
+
+    @Override
+    public void shutdown() {
+
+    }
+}
