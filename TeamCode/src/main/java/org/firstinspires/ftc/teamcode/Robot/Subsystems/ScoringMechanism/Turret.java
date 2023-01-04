263ae869ad1413423b25297ba1d46c5e9def8f13
Jacob Van Meter <jacobvm04@gmail.com>
2023-01-03 23:37:51 -0500

    added claw control to turrent and added some controlling methods

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/Subsystems/ScoringMechanism/Turret.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/Subsystems/ScoringMechanism/Turret.java
@@ -8,20 +8,23 @@ import org.firstinspires.ftc.teamcode.CommandFramework.Subsystem;
 
 public class Turret extends Subsystem {
 
+
+
 	MainScoringMechanism.MechanismStates state = MainScoringMechanism.MechanismStates.BEGIN;
 
-	Servo turretServo;
+	Servo turret;
 	Servo arm1;
 	Servo arm2;
+	Servo claw;
 
 	@Override
 	public void initAuto(HardwareMap hwMap) {
 
-		turretServo = hwMap.get(Servo.class, "turret");
+		turret = hwMap.get(Servo.class, "turret");
 		arm1 = hwMap.get(Servo.class,"arm1");
 		arm2 = hwMap.get(Servo.class,"arm2");
 		arm2.setDirection(Servo.Direction.REVERSE);
-
+		claw = hwMap.get(Servo.class, "claw");
 	}
 
 	@Override
@@ -37,7 +40,20 @@ public class Turret extends Subsystem {
 		this.state = state;
 	}
 
-	protected void setServoPositionSync(double position) {
+	public void setClawGrabbing(boolean grabbing) {
+		// TODO: Maybe add a different state for normally dropping cone from claw and dropping the cone in the outtake?
+		if (grabbing)
+			claw.setPosition(0); // set to value that best grabs cone
+		else
+			claw.setPosition(1); // set to value that best drops cone, should be as small as possible so that it fits between the outtake slides
+	}
+
+	protected void setTurretServoSync(double position) {
+		position = Range.clip(position,-1,1);
+		turret.setPosition(position);
+	}
+
+	protected void setArmPositionSync(double position) {
 		position = Range.clip(position,-1,1);
 		arm1.setPosition(position);
 		arm2.setPosition(1 - position);
