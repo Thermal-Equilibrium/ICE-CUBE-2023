6f527bf59bf9478099bfb51f05de090d3a99d6ed
Ayush <61765149+citricacid2@users.noreply.github.com>
2023-01-12 11:13:42 -0500

    fun "snaptogrid" odo

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/Commands/DrivetrainCommands/AlignViaPole.java
@@ -0,0 +1,53 @@
+package org.firstinspires.ftc.teamcode.Robot.Commands.DrivetrainCommands;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+
+import org.firstinspires.ftc.teamcode.CommandFramework.Command;
+import org.firstinspires.ftc.teamcode.Robot.Subsystems.Drivetrain;
+
+public class AlignViaPole extends Command {
+    Drivetrain drivetrain;
+    double distanceFromPole = 3; //inches
+
+    public AlignViaPole(Drivetrain drivetrain) {
+        super(drivetrain);
+        this.drivetrain = drivetrain;
+    }
+
+    @Override
+    public void init() {
+        Pose2d pose =  drivetrain.getPose();
+        // detect the nearest pole using the current position and the angle
+        // the field is -70 to 70 in both axis
+        // the poles are 23.3 inches apart
+
+        // get the nearest center of pole to the robot
+        double x = Math.round(pose.getX() / 23.3) * 23.3;
+        double y = Math.round(pose.getY() / 23.3) * 23.3;
+
+        // get the angle of the robot
+        double robotAngle = pose.getHeading();
+
+        // offset the robot position by the distance from the pole
+        double xOff = x + distanceFromPole * Math.cos(robotAngle);
+        double yOff = y + distanceFromPole * Math.sin(robotAngle);
+
+        // update robot pose
+        drivetrain.setPose(new Pose2d(xOff, yOff, robotAngle));
+    }
+
+    @Override
+    public void periodic() {
+
+    }
+
+    @Override
+    public boolean completed() {
+        return true;
+    }
+
+    @Override
+    public void shutdown() {
+
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/Commands/ScoringCommands/primitiveMovements/MoveVerticalExtension.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/Commands/ScoringCommands/primitiveMovements/MoveVerticalExtension.java
@@ -1,21 +1,50 @@
 package org.firstinspires.ftc.teamcode.Robot.Commands.ScoringCommands.primitiveMovements;
 
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+
 import org.firstinspires.ftc.teamcode.CommandFramework.Command;
+import org.firstinspires.ftc.teamcode.Robot.Commands.DrivetrainCommands.AlignViaPole;
+import org.firstinspires.ftc.teamcode.Robot.Subsystems.Drivetrain;
 import org.firstinspires.ftc.teamcode.Robot.Subsystems.ScoringMechanism.VerticalExtension;
 
 public class MoveVerticalExtension extends Command {
 
 	VerticalExtension extension;
 	double desiredPosition;
+	Drivetrain drivetrain;
+	double distanceFromPole = 3; //inches
 
-	public MoveVerticalExtension(VerticalExtension extension, double desiredPosition) {
+	public MoveVerticalExtension(VerticalExtension extension, double desiredPosition, Drivetrain drivetrain) {
+		super(drivetrain);
 		this.extension = extension;
 		this.desiredPosition = desiredPosition;
+
+		this.drivetrain = drivetrain;
 	}
 
 	@Override
 	public void init() {
 		extension.updateTargetPosition(desiredPosition);
+//		if (desiredPosition == VerticalExtension.IN_POSITION) {
+//			Pose2d pose =  drivetrain.getPose();
+//			// detect the nearest pole using the current position and the angle
+//			// the field is -70 to 70 in both axis
+//			// the poles are 23.3 inches apart
+//
+//			// get the nearest center of pole to the robot
+//			double x = Math.round(pose.getX() / 23.3) * 23.3;
+//			double y = Math.round(pose.getY() / 23.3) * 23.3;
+//
+//			// get the angle of the robot
+//			double robotAngle = pose.getHeading();
+//
+//			// offset the robot position by the distance from the pole
+//			double xOff = x + distanceFromPole * Math.cos(robotAngle);
+//			double yOff = y + distanceFromPole * Math.sin(robotAngle);
+//
+//			// update robot pose
+//			drivetrain.setPose(new Pose2d(xOff, yOff, robotAngle));
+//		}
 	}
 
 	@Override
