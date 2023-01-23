ec40460e4e4a7f7a9b7f4e29f6295c9af970dda8
Ben Caunt <bdcaunt@gmail.com>
2023-01-23 11:43:40 -0500

    added MeepMeep file for Sheerabdhi's mid pole auto

---
+++ MeepMeepTesting/src/main/java/com/thermalequilibrium/meepmeeptesting/SheerabdhiMIDSideAuto.java
@@ -0,0 +1,57 @@
+package com.thermalequilibrium.meepmeeptesting;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.noahbres.meepmeep.MeepMeep;
+import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
+import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+
+public class SheerabdhiMIDSideAuto {
+	public static double MAX_VEL = 65;
+	public static double MAX_ACCEL = 65;
+	public static double MAX_ANG_VEL = Math.toRadians(150);
+	public static double MAX_ANG_ACCEL = Math.toRadians(120);
+
+	public static void main(String[] args) {
+		MeepMeep meepMeep = new MeepMeep(800);
+		Pose2d startPose = new Pose2d(-36, 66.5,Math.toRadians(-90));
+		Pose2d exampleEnd = new Pose2d(0, 0,Math.toRadians(0));
+
+
+
+		RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
+				// Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+				.setConstraints(MAX_VEL, MAX_ACCEL, MAX_ANG_VEL, MAX_ANG_ACCEL, 12)
+				.followTrajectorySequence(drive ->
+						drive.trajectorySequenceBuilder(startPose)
+								.splineToSplineHeading(exampleEnd,Math.toRadians(0))
+								.build()
+
+				);
+
+		meepMeep.setBackground(MeepMeep.Background.FIELD_POWERPLAY_KAI_DARK)
+				.setDarkMode(true)
+				.setBackgroundAlpha(0.95f)
+				.addEntity(myBot.setDimensions(13,17))
+
+				.start();
+	}
+
+	public static Pose2d shiftRobotRelative(Pose2d poseGlobal, double robotRelativeX, double robotRelativeY) {
+		// unit vector of the global Pose
+		Vector2d globalVec = poseGlobal.vec();
+		Vector2d globalVecUnit = poseGlobal.headingVec();//globalVec.div(globalVec.norm());  // vec / mag = unit vector
+		// orthogonal vec
+		Vector2d orthogonalVec = new Vector2d(-globalVecUnit.getY(),globalVecUnit.getX());
+
+		Vector2d combinedVector = globalVec.plus(globalVecUnit.times(robotRelativeX))
+				.plus(orthogonalVec.times(robotRelativeY));
+		return new Pose2d(combinedVector, poseGlobal.getHeading());
+	}
+	public static double calculateTangent(Pose2d initialPosition, Pose2d finalPosition) {
+		double xd = initialPosition.getX() - finalPosition.getX();
+		double yd = initialPosition.getY() - finalPosition.getY();
+		return Math.atan2(yd,xd) - Math.PI;
+	}
+
+}
