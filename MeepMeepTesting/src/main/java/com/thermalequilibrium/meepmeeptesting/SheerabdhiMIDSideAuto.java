ec802f4e3d6626366436e534c58b9cc268e166ee
Enter21 <sheerabdhi@gmail.com>
2023-01-23 19:17:46 -0500

    Added capability to move from tile A2 and park in each respective parking zone to "SheerabdhiMIDSideAuto". Also changed to make the robot face the medium pole in each scenario.

--- MeepMeepTesting/src/main/java/com/thermalequilibrium/meepmeeptesting/SheerabdhiMIDSideAuto.java
+++ MeepMeepTesting/src/main/java/com/thermalequilibrium/meepmeeptesting/SheerabdhiMIDSideAuto.java
@@ -15,31 +15,43 @@ public class SheerabdhiMIDSideAuto {
 	public static double MAX_ANG_ACCEL = Math.toRadians(120);
 
 	public static void main(String[] args) {
-		int parkingLocation = 2;
+		int parkingLocation = 1;
 
 		MeepMeep meepMeep = new MeepMeep(800);
 		Pose2d startPose = new Pose2d(-36, 66.5,Math.toRadians(-90));
 		Pose2d exampleEnd = new Pose2d(0, 0,Math.toRadians(0));
-		Pose2d goToPoleHigh = new Pose2d(-35, 14, Math.toRadians(-70));
-		Pose2d rotateFaceRight = new Pose2d(-36, 12.5, Math.toRadians(0));
+		Pose2d goToPole23 = new Pose2d(-35, 14, Math.toRadians(-70));
+		Pose2d goToPole1 = new Pose2d(-35, 8, Math.toRadians(-90));
+		Pose2d rotateFaceMedium23 = new Pose2d(-36, 12.5, Math.toRadians(45));
+		Pose2d rotateFaceMedium1 = new Pose2d(-36, 6, Math.toRadians(50));
 		Pose2d goToPark1 = new Pose2d(-12.5, 12.5, Math.toRadians(0));
-		Pose2d goToPark3 = new Pose2d(-58, 12.5, Math.toRadians(0));
-		Pose2d goToPark2 = new Pose2d(-35, 12.5, Math.toRadians(0));
+		Pose2d goToPark3 = new Pose2d(-58, 9, Math.toRadians(0));
+		Pose2d goToPark2 = new Pose2d(-35, 12.5, Math.toRadians(45));
 
 		HashMap<Integer, Pose2d> parking = new HashMap<Integer, Pose2d>();
+		HashMap<Integer, Pose2d> toScoring = new HashMap<Integer, Pose2d>();
+		HashMap<Integer, Pose2d> rotation = new HashMap<Integer, Pose2d>();
 
 		parking.put(1, goToPark1);
 		parking.put(3, goToPark3);
 		parking.put(2, goToPark2);
+
+		toScoring.put(1, goToPole1);
+		toScoring.put(2, goToPole23);
+		toScoring.put(3, goToPole23);
+
+		rotation.put(1, rotateFaceMedium1);
+		rotation.put(2, rotateFaceMedium23);
+		rotation.put(3, rotateFaceMedium23);
 
 		RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
 				// Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
 				.setConstraints(MAX_VEL, MAX_ACCEL, MAX_ANG_VEL, MAX_ANG_ACCEL, 12)
 				.followTrajectorySequence(drive ->
 						drive.trajectorySequenceBuilder(startPose)
-								.splineToSplineHeading(goToPoleHigh,Math.toRadians(270))
-								.splineToLinearHeading(rotateFaceRight,Math.toRadians(0))
-								.splineToSplineHeading(parking.get(parkingLocation),Math.toRadians(0))
+								.splineToSplineHeading(toScoring.get(parkingLocation),Math.toRadians(270))
+								.splineToLinearHeading(rotation.get(parkingLocation),Math.toRadians(0))
+								.splineToLinearHeading(parking.get(parkingLocation),Math.toRadians(0))
 								.build()
 				);
 
